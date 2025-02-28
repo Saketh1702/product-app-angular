b13c9cd08d2080c655e558f7b5d342558d5d3a95
Saketh1702 <angirekulasaketh@gmail.com>
2025-02-27 22:55:47 -0700

    added ProductCardComponent as per requirements

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../core/models/product';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
+  @Input() product!: Product;
+  @Output() productSelected = new EventEmitter<number>();
 
+  onProductSelect(): void {
+    this.productSelected.emit(this.product.id);
+  }
 }
