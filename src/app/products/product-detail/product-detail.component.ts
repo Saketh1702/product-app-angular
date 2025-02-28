b4bd322c81774f4840a3176b6a9e848973cbe8ac
Saketh1702 <angirekulasaketh@gmail.com>
2025-02-27 23:11:04 -0700

    Added routing & Navigation for ProductDetailComponent

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Product } from '../../core/models/product';
+import { ProductService } from '../../core/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product$!: Observable<Product | undefined>;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) { }
 
+  ngOnInit(): void {
+    this.product$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = Number(params.get('id'));
+        return this.productService.getProductById(id);
+      })
+    );
+  }
+
+  goBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
